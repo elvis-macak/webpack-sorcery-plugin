307823c169d195e57b85e01a003f8c9bcbf9ee05
elvis <elvis@lnmpy.com>
2017-10-20 19:23:51 +0800

    do publish

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import sorcery from 'sorcery';
 
-const DEFAULT_INCLUDE = /\.js$|\.map$/;
+const DEFAULT_INCLUDE = /\.js$/;
+const DEFAULT_EXCLUDE = /vendor/;
 
-export default class SorceryPlugin {
-  constructor(options) {
-    this.include = options.include || DEFAULT_INCLUDE
-    this.exclude = options.exclude
+module.exports = class SorceryPlugin {
+  constructor(options = {}) {
+    this.include = options.include || DEFAULT_INCLUDE;
+    this.exclude = options.exclude || DEFAULT_EXCLUDE;
   }
 
   apply(compiler) {
@@ -23,24 +24,24 @@ export default class SorceryPlugin {
           return {
             name,
             path: compilation.assets[name].existsAt
-          }
+          };
         }
-        return null
+        return null;
       })
       .filter(i => i);
   }
 
   isIncludeOrExclude(filename) {
-    const isIncluded = this.include ? this.include.test(filename) : true
-    const isExcluded = this.exclude ? this.exclude.test(filename) : false
-    return isIncluded && !isExcluded
+    const isIncluded = this.include ? this.include.test(filename) : true;
+    const isExcluded = this.exclude ? this.exclude.test(filename) : false;
+    return isIncluded && !isExcluded;
   }
 
   sorceryFiles(files) {
     return Promise.all(files.map(({
       path,
       name
-    }) => sorcery.load(path).then((chain) => chain.write())))
+    }) => sorcery.load(path).then((chain) => chain.write())));
   }
 
 }
